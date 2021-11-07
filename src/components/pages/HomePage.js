cc5cfe3360bf18f08d50af1edef5c38b8c5c992e
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2021-11-07 10:05:28 -0800

    add

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import {Button} from 'semantic-ui-react';
 import './beamlogo.png';
 
 const HomePage = () => (
 <div className="ui container">
-  {/* <img src={"./beamlogo.png"}/> */}
   <h1>Home Page</h1>
   <Link to='/login' className="button"> Login </Link><br/>
   <Link to='/registration' className ="button"> Registration</Link>
