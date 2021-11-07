ae9ec680133786c409d468b24aa5baed73d5a9d3
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2021-11-07 10:02:07 -0800

    add

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,7 +5,7 @@ import './beamlogo.png';
 
 const HomePage = () => (
 <div className="ui container">
-  <img src={"./beamlogo.png"}/>
+  {/* <img src={"./beamlogo.png"}/> */}
   <h1>Home Page</h1>
   <Link to='/login' className="button"> Login </Link><br/>
   <Link to='/registration' className ="button"> Registration</Link>
